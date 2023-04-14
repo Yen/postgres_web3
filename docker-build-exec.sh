beefdbd4a03be389b2f940914725776dbf5cc624
George Bott <george@bott.gg>
2023-04-14 19:36:48 +0100

    Added docker-build-exec.sh

---
+++ docker-build-exec.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# build the docker image
+IMAGE=$(docker build -q .)
+# start a detached container
+CONTAINER=$(docker run -e POSTGRES_PASSWORD=g -d $IMAGE)
+# attach psql to it
+docker exec -it -u postgres $CONTAINER bash -c "until pg_isready -q; do sleep 1; done; psql"
+# delete the container
+docker rm -f $CONTAINER
