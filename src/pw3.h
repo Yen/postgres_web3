07ae07947c86708ed3298eb8ea32c9d994d51cfd
George Bott <george@bott.gg>
2023-04-21 16:45:56 +0100

    Fixed int128/uint128 min/max macros

--- src/pw3.h
+++ src/pw3.h
@@ -18,8 +18,8 @@ static_assert(sizeof(pw3_int128) == 16);
 
 pw3_int128 *pw3_int128_palloc(pw3_int128 value);
 
-#define PW3_INT128_MAX (((pw3_int128)INT64_MAX << 64) | (pw3_int128)UINT64_MAX)
-#define PW3_INT128_MIN (-(PW3_INT128_MAX - 1))
+#define PW3_INT128_MAX ((((pw3_int128)INT64_MAX) << 64) | (pw3_int128)UINT64_MAX)
+#define PW3_INT128_MIN (~PW3_INT128_MAX)
 
 #define PW3_GETARG_INT128_P(n) ((pw3_int128 *)PG_GETARG_DATUM(n))
 #define PW3_RETURN_INT128_P(x) return ((Datum)(x))
@@ -31,7 +31,7 @@ static_assert(sizeof(pw3_uint128) == 16);
 
 pw3_uint128 *pw3_uint128_palloc(pw3_uint128 value);
 
-#define PW3_UINT128_MAX (((pw3_uint128)UINT64_MAX << 64) | (pw3_uint128)UINT64_MAX)
+#define PW3_UINT128_MAX (~pw3_uint128(0))
 #define PW3_UINT128_MIN 0
 
 #define PW3_GETARG_UINT128_P(n) ((pw3_uint128 *)PG_GETARG_DATUM(n))
